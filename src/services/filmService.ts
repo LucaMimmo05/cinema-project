3188c4cc02fc8298b6a7614dc7d15a311369744c
Luca Mimmo <171295375+LucaMimmo05@users.noreply.github.com>
2026-05-11 10:48:55 +0200

    Implement api to fetch single film

--- src/services/filmService.ts
+++ src/services/filmService.ts
@@ -1,8 +1,21 @@
         const API_URL = process.env.NEXT_PUBLIC_API_URL;
        
-        export const getAllFilms = async () => {
+    export const getAllFilms = async () => {
         try {
             const response = await fetch(API_URL + "/films" as string);
+
+             const body = await response.json();
+            console.log(body);
+             return body;
+        }
+        catch {
+            throw new Error("Failed to fetch");
+        }
+    }
+
+    export const getSingleFilm = async (id : number) => {
+        try {
+            const response = await fetch(API_URL + "/films/" + id as string);
 
              const body = await response.json();
             console.log(body);
