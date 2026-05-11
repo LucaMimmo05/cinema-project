40b9588d923836e5682a073b01797d905ce41989
Vladdddy <vlad00anonimous00@gmail.com>
2026-05-11 12:52:39 +0200

    Improve code formatting and structure

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "example.com", // replace with your actual domain
+                pathname: "/posters/**", // optional: restrict to a specific path
+            },
+        ],
+    },
 };
 
 export default nextConfig;
--- src/app/films/page.tsx
+++ src/app/films/page.tsx
@@ -1,27 +1,25 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 import Input from "../../components/ui/Input";
 import { Film as FilmInterface } from "@/src/interfaces/filmInterface";
 import { getAllFilms } from "@/src/services/filmService";
-import Film  from "@/src/components/ui/Film";
+import Film from "@/src/components/ui/Film";
 
 export default function page() {
     const [films, setFilms] = useState<FilmInterface[]>([]);
 
     useEffect(() => {
-    const load = async () => {
-        const films = await getAllFilms();
-        setFilms(films);
-    };
-    load();
+        const load = async () => {
+            const films = await getAllFilms();
+            setFilms(films);
+        };
+        load();
     }, []);
     return (
-        <section className="flex h-screen w-full items-start justify-between">
-            <div className="flex flex-col items-start justify-start bg-gray-50 border border-(--bento-stroke) p-4 w-80 h-screen">
-                <h1 className="text-2xl font-medium text-gray-600 border-b border-gray-300 pb-2 w-full">
-                    Filtri
-                </h1>
+        <section className="flex h-screen w-full items-start justify-between p-10 gap-10">
+            <div className="flex flex-col items-start justify-start h-screen min-w-60">
+                <h1 className="text-2xl font-medium text-gray-600">Filtri</h1>
                 <div className="flex flex-col items-start justify-start mt-10 w-full">
                     <label
                         className="text-gray-600 text-sm mb-2"
@@ -58,14 +56,12 @@ export default function page() {
                 </div>
             </div>
 
-            <div className="w-full flex-1 p-10">
+            <div className="w-full flex-1">
                 <Input type="searchbar" placeholder="Cerca un film..." />
 
                 <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
                     {films.map((film) => {
-                        return (
-                            <Film key={film.id} film={film}/>
-                        )
+                        return <Film key={film.id} film={film} />;
                     })}
                 </div>
             </div>
