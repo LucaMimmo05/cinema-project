7b0fcbeb1fd34dc2804dbf1637e328a5f506155c
Luca Mimmo <171295375+LucaMimmo05@users.noreply.github.com>
2026-05-11 10:41:29 +0200

    Delete test fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
-"use client"
-import { useEffect } from "react";
-import { getAllFilms } from "../services/filmService";
-
 export default function Home() {
-    useEffect(() => {
-       getAllFilms();
-    }, [] )
+  
     return <section>Homepage</section>;
 }
