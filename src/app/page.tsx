fa9d63e9dfc7bcf0af00a84f5498f4b2f7689aa0
Luca Mimmo <171295375+LucaMimmo05@users.noreply.github.com>
2026-05-11 10:40:31 +0200

    Implemented first api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+"use client"
+import { useEffect } from "react";
+import { getAllFilms } from "../services/filmService";
+
 export default function Home() {
+    useEffect(() => {
+       getAllFilms();
+    }, [] )
     return <section>Homepage</section>;
 }
---
+++ src/services/filmService.ts
@@ -0,0 +1,14 @@
+        const API_URL = process.env.NEXT_PUBLIC_API_URL;
+       
+        export const getAllFilms = async () => {
+        try {
+            const response = await fetch(API_URL + "/films" as string);
+
+             const body = await response.json();
+            console.log(body);
+             return body;
+        }
+        catch {
+            throw new Error("Failed to fetch");
+        }
+    }
